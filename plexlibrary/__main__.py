378c2208861d36d8ac0b2ca444630bea5ef6e595
adamgot <26773908+adamgot@users.noreply.github.com>
2017-09-29 23:32:02 +0200

    Initial restructure

---
+++ plexlibrary/__main__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from plexlibrary import main
+main()
+
---
+++ plexlibrary/plexlibrary.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Automated Plex library utility
+
+This utility creates or maintains a Plex library
+based on a configuration recipe.
+
+Disclaimer:
+    Use at your own risk! I am not responsible
+    for damages to your Plex server or libraries.
+"""
+
+import argparse
+import sys
+
+from recipe import Recipe
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('recipe', help='Create a library using this recipe')
+    parser.add_argument('-s', '--sort-only', action='store_true', help='Only sort the library')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    r = Recipe(args.recipe)
+    r.run(args.sort_only)
