d6ba910c16cc8c701aa838fab65ce5b9ce0a35ff
adamgot <26773908+adamgot@users.noreply.github.com>
2017-09-30 21:29:53 +0200

    trakt: require max_age

--- plexlibrary/traktutils.py
+++ plexlibrary/traktutils.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import trakt
 
 
@@ -8,11 +10,12 @@ class Trakt(object):
         self.trakt = trakt
         self.trakt_core = trakt.core.Core()
 
-    def add_movies(url, movie_list=None, movie_ids=None):
+    def add_movies(url, movie_list=None, movie_ids=None, max_age=0):
         if not movie_list:
             movie_list = []
         if not movie_ids:
             movie_ids = []
+        curyear = datetime.datetime.now().year
         print(u"Retrieving the trakt list: {}".format(url))
         movie_data = self.trakt_core._handle_request('get', url)
         for m in movie_data:
@@ -20,8 +23,8 @@ class Trakt(object):
             if m['movie']['ids']['imdb'] in movie_ids:
                 continue
             # Skip old movies
-            if self.recipe.MAX_AGE != 0 \
-                    and (curyear - (self.recipe.MAX_AGE - 1)) > int(m['movie']['year']):
+            if max_age != 0 \
+                    and (curyear - (max_age - 1)) > int(m['movie']['year']):
                 continue
             movie_list.append({
                 'id': m['movie']['ids']['imdb'],
@@ -37,11 +40,12 @@ class Trakt(object):
 
         return (movie_list, movie_ids)
 
-    def add_shows(url, show_list=None, show_ids=None):
+    def add_shows(url, show_list=None, show_ids=None, max_age=0):
         if not show_list:
             show_list = []
         if not show_ids:
             show_ids = []
+        curyear = datetime.datetime.now().year
         print(u"Retrieving the trakt list: {}".format(url))
         show_data = self.trakt_core._handle_request('get', url)
         for m in show_data:
@@ -49,8 +53,8 @@ class Trakt(object):
             if m['show']['ids']['imdb'] in show_ids:
                 continue
             # Skip old shows
-            if self.recipe.MAX_AGE != 0 \
-                    and (curyear - (self.recipe.MAX_AGE - 1)) > int(m['show']['year']):
+            if max_age != 0 \
+                    and (curyear - (max_age - 1)) > int(m['show']['year']):
                 continue
             show_list.append({
                 'id': m['show']['ids']['imdb'],
@@ -71,11 +75,11 @@ class Trakt(object):
 
         return (show_list, show_ids)
 
-    def add_items(item_type, url, item_list=None, item_ids=None):
+    def add_items(item_type, url, item_list=None, item_ids=None, max_age=0):
         if item_type == 'movie':
             return self.add_movies(url, movie_list=item_list,
-                                   movie_ids=item_ids)
+                                   movie_ids=item_ids, max_age=max_age)
         elif item_type == 'tv':
             return self.add_shows(url, show_list=item_list,
-                                  show_ids=item_ids)
+                                  show_ids=item_ids, max_age=max_age)
 
