2038548f922a7175158be3bb1f028b2cb4df814f
adamgot <26773908+adamgot@users.noreply.github.com>
2017-09-30 16:29:00 +0200

    Added recipe parser

---
+++ plexlibrary/recipes.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+
+from utils import YAMLBase
+
+
+class RecipeParser(YAMLBase):
+    def __init__(self, name, directory=None):
+        self.name = name.strip('.yml')
+        recipe_file = self.name + '.yml'
+        # FIXME?
+        if not directory:
+            parent_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__),
+                os.path.pardir)))
+            directory = os.path.join(parent_dir, 'recipes')
+
+        filepath = os.path.join(directory, recipe_file)
+
+        super(RecipeParser, self).__init__(filepath)
+
+    def print(self):
+        print(self.data)
+
+
+def list_recipes(directory=None):
+    raise NotImplementedError()
+
