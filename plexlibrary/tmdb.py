728515bc213a97cf8ef78a7ce25a5e289256c035
adamgot <26773908+adamgot@users.noreply.github.com>
2017-09-30 23:59:30 +0200

    tmdb: converted to class

--- plexlibrary/tmdb.py
+++ plexlibrary/tmdb.py
@@ -1,105 +1,102 @@
 # -*- coding: utf-8 -*-
+import json
 import shelve
+import time
 
 import requests
 
-TMDB_REQUEST_COUNT = 0  # DO NOT CHANGE
 
+class TMDb(object):
+    api_key = None
+    cache_file = None
+    request_count = 0
+    def __init__(self, api_key, cache_file=None):
+        self.api_key = api_key
+        if cache_file:
+            self.cache_file = cache_file
+        else:
+            self.cache_file = 'tmdb_details.shelve'
 
-def get_imdb_id(tmdb_id, library_type='movie'):
-    global TMDB_REQUEST_COUNT
+    def get_imdb_id(self, tmdb_id, library_type='movie'):
+        if library_type not in ('movie', 'tv'):
+            raise Exception("Library type should be 'movie' or 'tv'")
 
-    if library_type not in ('movie', 'tv'):
-        raise Exception("Library type should be 'movie' or 'tv'")
+        # Use cache
+        cache = shelve.open(cache_file)
+        if cache.has_key(str(tmdb_id)):
+            item = cache[str(tmdb_id)]
+            cache.close()
+            return item.get('imdb_id')
 
-    # Use cache
-    cache = shelve.open(config.TMDB_CACHE_FILE)
-    if cache.has_key(str(tmdb_id)):
-        item = cache[str(tmdb_id)]
-        cache.close()
-        return item.get('imdb_id')
+        # Wait 10 seconds for the TMDb rate limit
+        if self.request_count >= 40:
+            print(u"Waiting 10 seconds for the TMDb rate limit...")
+            time.sleep(10)
+            self.request_count = 0
 
-    if not config.TMDB_API_KEY:
-        cache.close()
-        return None
+        params = {
+            'api_key': self.api_key,
+        }
 
-    # Wait 10 seconds for the TMDb rate limit
-    if TMDB_REQUEST_COUNT >= 40:
-        print(u"Waiting 10 seconds for the TMDb rate limit...")
-        time.sleep(10)
-        TMDB_REQUEST_COUNT = 0
-
-    params = {
-        'api_key': config.TMDB_API_KEY,
-    }
-
-    if library_type == 'movie':
-        url = "https://api.themoviedb.org/3/movie/{tmdb_id}".format(
-            tmdb_id=tmdb_id)
-    else:
-        url = "https://api.themoviedb.org/3/tv/{tmdb_id}/external_ids".format(
-            tmdb_id=tmdb_id)
-    r = requests.get(url, params=params)
-
-    TMDB_REQUEST_COUNT += 1
-
-    if r.status_code == 200:
-        item = json.loads(r.text)
-        item['cached'] = int(time.time())
-        cache[str(tmdb_id)] = item
-        cache.close()
-        return item.get('imdb_id')
-    else:
-        cache.close()
-        return None
-
-
-def get_details(tmdb_id, library_type='movie'):
-    global TMDB_REQUEST_COUNT
-
-    if library_type not in ('movie', 'tv'):
-        raise Exception("Library type should be 'movie' or 'tv'")
-
-    # Use cache
-    cache = shelve.open(config.TMDB_CACHE_FILE)
-    if cache.has_key(str(tmdb_id)) and \
-            (cache[str(tmdb_id)]['cached'] + 3600 * 24) > int(time.time()):
-        item = cache[str(tmdb_id)]
-        cache.close()
-        return item
-
-    if not config.TMDB_API_KEY:
-        cache.close()
-        return None
-
-    # Wait 10 seconds for the TMDb rate limit
-    if TMDB_REQUEST_COUNT >= 40:
-        print(u"Waiting 10 seconds for the TMDb rate limit...")
-        time.sleep(10)
-        TMDB_REQUEST_COUNT = 0
-
-    params = {
-        'api_key': config.TMDB_API_KEY,
-    }
-
-    if library_type == 'movie':
-        params['append_to_response'] = 'release_dates'
-        url = "https://api.themoviedb.org/3/movie/{tmdb_id}".format(
+        if library_type == 'movie':
+            url = "https://api.themoviedb.org/3/movie/{tmdb_id}".format(
                 tmdb_id=tmdb_id)
-    else:
-        url = "https://api.themoviedb.org/3/tv/{tmdb_id}".format(
+        else:
+            url = "https://api.themoviedb.org/3/tv/{tmdb_id}/external_ids".format(
                 tmdb_id=tmdb_id)
-    r = requests.get(url, params=params)
+        r = requests.get(url, params=params)
 
-    TMDB_REQUEST_COUNT += 1
+        self.request_count += 1
 
-    if r.status_code == 200:
-        item = json.loads(r.text)
-        item['cached'] = int(time.time())
-        cache[str(tmdb_id)] = item
-        cache.close()
-        return item
-    else:
-        cache.close()
-        return None
+        if r.status_code == 200:
+            item = json.loads(r.text)
+            item['cached'] = int(time.time())
+            cache[str(tmdb_id)] = item
+            cache.close()
+            return item.get('imdb_id')
+        else:
+            return None
+
+    def get_details(self, tmdb_id, library_type='movie'):
+        if library_type not in ('movie', 'tv'):
+            raise Exception("Library type should be 'movie' or 'tv'")
+
+        # Use cache
+        cache = shelve.open(self.cache_file)
+        if (cache.has_key(str(tmdb_id)) and
+                (cache[str(tmdb_id)]['cached'] + 3600 * 24)
+                 > int(time.time())):
+            item = cache[str(tmdb_id)]
+            cache.close()
+            return item
+
+        # Wait 10 seconds for the TMDb rate limit
+        if self.request_count >= 40:
+            print(u"Waiting 10 seconds for the TMDb rate limit...")
+            time.sleep(10)
+            self.request_count = 0
+
+        params = {
+            'api_key': self.api_key,
+        }
+
+        if library_type == 'movie':
+            params['append_to_response'] = 'release_dates'
+            url = "https://api.themoviedb.org/3/movie/{tmdb_id}".format(
+                    tmdb_id=tmdb_id)
+        else:
+            url = "https://api.themoviedb.org/3/tv/{tmdb_id}".format(
+                    tmdb_id=tmdb_id)
+        r = requests.get(url, params=params)
+
+        self.request_count += 1
+
+        if r.status_code == 200:
+            item = json.loads(r.text)
+            item['cached'] = int(time.time())
+            cache[str(tmdb_id)] = item
+            cache.close()
+            return item
+        else:
+            return None
 
