c6565581bf296c980ff57c279e4ccb0a9e467454
adamgot <26773908+adamgot@users.noreply.github.com>
2017-10-01 15:03:53 +0200

    Trakt workaround

--- plexlibrary/traktutils.py
+++ plexlibrary/traktutils.py
@@ -1,14 +1,46 @@
 # -*- coding: utf-8 -*-
 import datetime
+import json
 
+import requests
 import trakt
 
 
 class Trakt(object):
     def __init__(self, username, client_id='', client_secret=''):
+        self.username = username
+        self.client_id = client_id
+        self.client_secret = client_secret
         trakt.init(username, client_id=client_id, client_secret=client_secret)
         self.trakt = trakt
         self.trakt_core = trakt.core.Core()
+
+    def _handle_request(self, method, url, data=None, oauth=False):
+        """Stolen from trakt.core to support optional OAUTH operations
+        :todo: Fix trakt
+        """
+        headers = {'Content-Type': 'application/json',
+                   'trakt-api-version': '2'}
+        #self.logger.debug('%s: %s', method, url)
+        headers['trakt-api-key'] = self.client_id
+        if oauth:
+            headers['Authorization'] = 'Bearer {0}'.format(self.oauth_token)
+        #self.logger.debug('headers: %s', str(headers))
+        #self.logger.debug('method, url :: %s, %s', method, url)
+        if method == 'get':  # GETs need to pass data as params, not body
+            response = requests.request(method, url, params=data,
+                                        headers=headers)
+        else:
+            response = requests.request(method, url, data=json.dumps(data),
+                                        headers=headers)
+        #self.logger.debug('RESPONSE [%s] (%s): %s',
+        #    method, url, str(response))
+        if response.status_code in self.trakt_core.error_map:
+            raise self.trakt_core.error_map[response.status_code]()
+        elif response.status_code == 204:  # HTTP no content
+            return None
+        json_data = json.loads(response.content.decode('UTF-8', 'ignore'))
+        return json_data
 
     def add_movies(self, url, movie_list=None, movie_ids=None, max_age=0):
         if not movie_list:
@@ -17,7 +49,7 @@ class Trakt(object):
             movie_ids = []
         curyear = datetime.datetime.now().year
         print(u"Retrieving the trakt list: {}".format(url))
-        movie_data = self.trakt_core._handle_request('get', url)
+        movie_data = self._handle_request('get', url)
         for m in movie_data:
             # Skip already added movies
             if m['movie']['ids']['imdb'] in movie_ids:
@@ -47,7 +79,7 @@ class Trakt(object):
             show_ids = []
         curyear = datetime.datetime.now().year
         print(u"Retrieving the trakt list: {}".format(url))
-        show_data = self.trakt_core._handle_request('get', url)
+        show_data = self._handle_request('get', url)
         for m in show_data:
             # Skip already added shows
             if m['show']['ids']['imdb'] in show_ids:
