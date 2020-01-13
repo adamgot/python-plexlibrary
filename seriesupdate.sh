d1322153853908451085ed7cf948c707822f270f
Jake <hawkinzzz@hotmail.co.uk>
2020-01-13 11:51:32 +0000

    Create seriesupdate.sh

---
+++ seriesupdate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+
+#TV Documentaries
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-docu-65-rating
+sleep 30s
+
+#HBO
+traktarr shows -t https://trakt.tv/users/porkie16/lists/hbo-network-65
+sleep 30s
+
+#Discovery
+traktarr shows -t https://trakt.tv/users/porkie16/lists/discovery-network-65-rating
+sleep 30s
+
+#Sky Atlantic
+traktarr shows -t https://trakt.tv/users/porkie16/lists/sky-atlantic-tv-rating-65
+sleep 30s
+
+#Comedy
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-comedy-65-rating
+sleep 30s
+
+#Sci-Fi
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-sci-fi-65-rating
+sleep 30s
+
+#Drama
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-drama-65-rating
+sleep 30s
+
+#Super Hero
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-superhero-65-rating
+sleep 30s
+
+#Crime
+traktarr shows -t https://trakt.tv/users/porkie16/lists/tv-crime-65-rating
+sleep 30s
+
+#Netflix
+traktarr shows -t https://trakt.tv/users/porkie16/lists/netflixs-tv-shows-65-rating
+sleep 30s
+
+#Apple
+traktarr shows -t https://trakt.tv/users/porkie16/lists/apple-tv-shows-65-rating
+sleep 30s
+
+#Disney
+traktarr shows -t https://trakt.tv/users/porkie16/lists/disney-tv-shows-65-rating
+sleep 30s
+
+#Amazon
+traktarr shows -t https://trakt.tv/users/porkie16/lists/amazon-tv-65-rating
+sleep 30s
+
+#Hulu
+traktarr shows -t https://trakt.tv/users/porkie16/lists/hulu-tv-rating-65
+sleep 30s
